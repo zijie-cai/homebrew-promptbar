56c55626af0e0df56c9117881bbca56aeba66197
Zijie Cai <leo20020208@gmail.com>
2026-03-25 14:30:16 -0400

    Update PromptBar cask to 1.0.3

--- Casks/promptbar.rb
+++ Casks/promptbar.rb
@@ -1,6 +1,6 @@
 cask "promptbar" do
-  version "1.0.2"
-  sha256 "64aaef2559ed2aabd124b8e6b2d647f17ddf810000aa303f5661904e7736a6b8"
+  version "1.0.3"
+  sha256 "15a303c9c56ab349b078004d84d38a47b7fe0d2637d7985da298c738a4187498"
 
   url "https://github.com/zijie-cai/PromptBar/releases/download/v#{version}/PromptBar.zip"
   name "PromptBar"
